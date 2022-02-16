ca2745c0130fc4d0ccba9a609c7a44e3c7f26ddd
tr-abhishek <81833060+tr-abhishek@users.noreply.github.com>
2022-02-17 04:30:48 +0530

    Update mapper.java

--- 3. Hadoop-Insurance/mapper.java
+++ 3. Hadoop-Insurance/mapper.java
@@ -6,9 +6,10 @@ import org.apache.hadoop.mapred.*;
 
 public class mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{
 	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException {
-        String valueString = value.toString();
-		String[] SingleCountryData = valueString.split(",");
-		values.collect(new Text(SingleCountryData[16]), new IntWritable(1));
+		
+        	String[] str = value.toString().split(",");
+		String county = str[2];
+		values.collect(new Text(county), new IntWritable(1));
 	}
 
 }
