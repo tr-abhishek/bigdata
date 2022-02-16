81dfbbbc1ad93ed101ae1b705acc276df2ba6f5d
tr-abhishek <81833060+tr-abhishek@users.noreply.github.com>
2022-02-17 05:00:14 +0530

    Update reducer.java

--- 3. Hadoop-Insurance/reducer.java
+++ 3. Hadoop-Insurance/reducer.java
@@ -7,10 +7,10 @@ import org.apache.hadoop.mapred.*;
 
 public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable>{
 		public void reduce(Text key, Iterator<IntWritable> value1, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException {
-            int frequencyForCountry = 0;
+            int county = 0;
             while (value1.hasNext()) {
-                frequencyForCountry += value1.next().get();
+                county += value1.next().get();
             }
-            values.collect(key, new IntWritable(frequencyForCountry));
+            values.collect(key, new IntWritable(county));
 	}
 }
