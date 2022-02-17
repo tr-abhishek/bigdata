89719eb575e7f25943c805321b719abe22be56bc
tr-abhishek <81833060+tr-abhishek@users.noreply.github.com>
2022-02-17 05:51:03 +0530

    Update mapper.java

--- 6. Hadoop-SalesRecord/mapper.java
+++ 6. Hadoop-SalesRecord/mapper.java
@@ -1,14 +1,27 @@
 package salesrecord;
-import java.io.IOException;
 
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 
-public class mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{
-	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException {
-        String valueString = value.toString();
-		String[] SingleCountryData = valueString.split(",");
-		values.collect(new Text(SingleCountryData[7]), new IntWritable(1));
-	}
+public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable> {
 
+    public void reduce(Text key, Iterator<IntWritable> value1, OutputCollector<Text,IntWritable> values,Reporter r) throws IOException {
+        String temp = key.toString();
+        if(temp.substring(0, 9) == "_country_"){
+            int total_sales = 0;
+            while(value1.hasNext()){
+                total_sales+=value1.next().get();
+            }
+            values.collect(key, new IntWritable(total_sales));
+        } else{
+            int payment_freq = 0;
+            while(value1.hasNext()){
+                payment_freq+=value1.next().get();
+            }
+            values.collect(key, new IntWritable(payment_freq));
+        }
+    }
 }
