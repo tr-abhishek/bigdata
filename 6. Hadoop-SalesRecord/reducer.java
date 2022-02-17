0bcb7b072bd221a56c0db4fadc6f2c9fd96a6e05
tr-abhishek <81833060+tr-abhishek@users.noreply.github.com>
2022-02-17 05:52:08 +0530

    Update reducer.java

--- 6. Hadoop-SalesRecord/reducer.java
+++ 6. Hadoop-SalesRecord/reducer.java
@@ -1,16 +1,27 @@
 package salesrecord;
-import java.io.IOException;
-import java.util.Iterator;
 
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 
-public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable>{
-		public void reduce(Text key, Iterator<IntWritable> value1, OutputCollector<Text, IntWritable> values, Reporter r) throws IOException {
-		int frequencyForCountry = 0;
-		while (value1.hasNext()) {
-			frequencyForCountry += value1.next().get();
-		}
-		values.collect(key, new IntWritable(frequencyForCountry));
-	}
+public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable> {
+
+    public void reduce(Text key, Iterator<IntWritable> value1, OutputCollector<Text,IntWritable> values,Reporter r) throws IOException {
+        String temp = key.toString();
+        if(temp.substring(0, 9) == "_country_"){
+            int total_sales = 0;
+            while(value1.hasNext()){
+                total_sales+=value1.next().get();
+            }
+            values.collect(key, new IntWritable(total_sales));
+        } else{
+            int payment_freq = 0;
+            while(value1.hasNext()){
+                payment_freq+=value1.next().get();
+            }
+            values.collect(key, new IntWritable(payment_freq));
+        }
+    }
 }
